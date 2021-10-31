ecec31c5a9dfcba463b6dcbfbaf4e4e99732dbba
unknown <tatkol2003@mail.ru>
2021-10-31 20:16:27 +0300

    task 2 Done

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,15 +7,29 @@ function calcShipping(sum, min, shipping) {
 
     let shippingSum; // создайте переменную shippingSum
 
-    if (productsSum == 0) {// если productsSum равно 0,
-        shippingSum = 0; // то shippingSum присвоить значение 0
-    }
+    //if (productsSum == 0) {// если productsSum равно 0,
+    //    shippingSum = 0; // то shippingSum присвоить значение 0
+    //}
 
-    if (productsSum >= freeShippingMinSum) {// если productsSum Больше или равна freeShippingMinSum,
-        shippingSum = 0;// то shippingSum присвоить значение 0
-    }
+    //if (productsSum >= freeShippingMinSum) {// если productsSum Больше или равна freeShippingMinSum,
+    //    shippingSum = 0;// то shippingSum присвоить значение 0
+    //}
+    
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+      }
 
-    if (productsSum > 0 || productsSum < freeShippingMinSum) {// если productsSum больше 0 и меньше freeShippingMinSum,
+
+// Ваш комментарий   (ниже) по строке не удалось понять.
+// Напишите, пожалуйста, пример!
+// В третьем условии используется логическое ИЛИ, 
+// которое будет переприсваивать значение, это действие будет приводить к ошибкам. 
+// Правильней было использовать логическое И, а ещё лучше использовать просто else. 
+// То есть присвоить либо одно значение, либо другое (if / else). 
+// Из-за этой ошибки отправляю задание на доработку. 
+// Так как из-за этой ошибки вычисления выполняются неверно.
+
+      if (productsSum > 0 || productsSum < freeShippingMinSum) {// если productsSum больше 0 и меньше freeShippingMinSum,
         shippingSum = shippingPrice; // то shippingSum присвоить значение shippingPrice
     } 
 
@@ -31,10 +45,12 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    let discountSum; // создайте переменную discountSum
-    if (productsSum >= discountMinSum) {// если productsSum больше или равно discountMinSum,
-        discountSum = discountPart / 100 * productsSum; // то присвойте discountSum значение discountPart процентов от productsSum,
-       } else { discountSum = 0; }// иначе присвойте discountSum значение 0
+    //let discountSum; // создайте переменную discountSum
+    // if (productsSum >= discountMinSum) {// если productsSum больше или равно discountMinSum,
+    //    discountSum = discountPart / 100 * productsSum; // то присвойте discountSum значение discountPart процентов от productsSum,
+    //   } else { discountSum = 0; }// иначе присвойте discountSum значение 0
+       
+       let discountSum = productsSum >= discountMinSum ? productsSum / 100 * discountPart : 0;
 
     // Конец решения задания №2.2.
 
@@ -56,9 +72,11 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     totalSum = totalSum + shippingSum; // прибавьте к totalSum значение shippingSum
 
-    let freeShipping; // создайте переменную freeShipping
+    //let freeShipping; // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
-    (shippingSum == 0) ? freeShipping = true : freeShipping = false;// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    //(shippingSum == 0) ? freeShipping = true : freeShipping = false;// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+   
+    let freeShipping = shippingSum == 0;
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
